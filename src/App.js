23f8f5a6aa680bdecc4956f8d7acf539eb4dca13
ThChursaard <jame.paladin26222@gmail.com>
2021-08-19 23:21:15 +0700

    shuffle question

--- src/App.js
+++ src/App.js
@@ -13,10 +13,27 @@ const answer = [
   'exercise'
 ]
 
-const qa = [{q:'Action that is againist the law', a:'crime'},
+const qa = shuffle([{q:'Action that is againist the law', a:'crime'},
             {q:'In the open air; Outside a building', a:'outdoor'},
             {q:'Physical activity that keep you fit', a:'exercise'}
-]
+])
+function shuffle(array) {
+  var currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
 function App() {
   const [input, setInput] = useState('');
