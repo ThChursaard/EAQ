1c144a1673499f77d93e2f1f0523fe16c1953c57
ThChursaard <jame.paladin26222@gmail.com>
2021-08-19 22:43:17 +0700

    answer function

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import React, { useState } from 'react';
 function App() {
   const [input, setInput] = useState('');
   const [score, setScore] = useState(0);
+  const checkAnswer = (answer) => {
+    if(answer == 'healthy'){
+      setScore(score+1);
+    }
+  }
   return (
     <div className="App">
       <div>
+      <p>Your Score:{score}</p>
       <p> What is the vocab : strong and well;not sick</p>
       <form>
         <label></label>
         <input onChange = {(e) => setInput(e.target.value)}  value ={input}/>
-        <button>Enter</button>
+        <button onClick = {(e) => {e.preventDefault()
+           checkAnswer(input)}}>Enter</button>
       </form>
     </div>
     </div>
