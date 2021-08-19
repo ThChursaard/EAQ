e4fed35a5d078082e06606a34b0f066e87065747
ThChursaard <jame.paladin26222@gmail.com>
2021-08-19 23:00:27 +0700

    add index

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-
+const question = [
+  'Action that is againist the law',
+  'In the open air; Outside a building',
+  'Physical activity that keep you fit'
+];
+const answer = [
+  'crime',
+  'outdoor',
+  'exercise'
+] 
 function App() {
   const [input, setInput] = useState('');
   const [score, setScore] = useState(0);
-  const checkAnswer = (answer) => {
-    if(answer == 'healthy'){
+  const [index, setIndex] = useState(0);
+  const checkAnswer = (myanswer) => {
+    if(myanswer == answer[index]){
       setScore(score+1);
     }
+    setIndex(index+1);
   }
   return (
     <div className="App">
       <div>
       <p>Your Score:{score}</p>
-      <p> What is the vocab : strong and well;not sick</p>
+      <p>{question[index]}</p>
       <form>
         <label></label>
         <input onChange = {(e) => setInput(e.target.value)}  value ={input}/>
