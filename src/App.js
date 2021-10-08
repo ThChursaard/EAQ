6ea229176247eab8d932763535a9065f29667c89
ThChursaard <jame.paladin26222@gmail.com>
2021-10-08 18:19:01 +0700

    fixworkingbutton

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { qa } from "./ques.js";
 import SelectQues from "./SelectQues.js";
 import { useEffect } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4001/";
+const ENDPOINT = "http://127.0.0.1:4001";
 
 function App() {
   const [input, setInput] = useState("");
