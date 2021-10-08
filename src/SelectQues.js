be90b071398f10f526af09693077957ea0c3d249
ThChursaard <jame.paladin26222@gmail.com>
2021-10-08 18:54:28 +0700

    fixsendquestion

--- src/SelectQues.js
+++ src/SelectQues.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-import {qa} from './ques.js';
-import { shuffle } from './ques.js';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { qa } from "./ques.js";
+import { shuffle } from "./ques.js";
+const ENDPOINT = "http://127.0.0.1:4001";
 
-const a = qa.map(c => c.a)
+const a = qa.map((c) => c.a);
+const sendQuestion = (question, answer) => {
+  fetch(`${ENDPOINT}/addquestion/?question=${question}&answer=${answer}`, { method: "GET" });
+};
 function SelectQues() {
-    const [question, setQuestion] = useState(shuffle(a));
+  const [question, setQuestion] = useState(shuffle(a));
   return (
     <div>
-        <button>{question[0]}</button>
-        <button>{question[1]}</button>
-        <button>{question[2]}</button>
-        <button>{question[3]}</button>
-        <button>{question[4]}</button>
+      {qa.map((c) => (
+        <button onClick = {(e) => sendQuestion(c.q,c.a)} > {c.a} </button> 
+      ))}
     </div>
-  )
+  );
 }
 
 export default SelectQues;
