719820736f3bbcb8080edf9ed347d4878fb26f2a
ThChursaard <jame.paladin26222@gmail.com>
2022-05-04 15:18:37 +0700

    add_endcredit

---
+++ src/endcredit.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+function EndCredit() {
+  return (
+    <div>
+      <h1> Thank you for participating. </h1>
+      <h2> If You found bug. Report https://github.com/ThChursaard/EAQ </h2>
+    </div>
+  );
+}
+
+export default EndCredit;
