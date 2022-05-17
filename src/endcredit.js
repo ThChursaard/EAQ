89a1e4c5f366c33c79c9d852e9b873e18c4bd168
ThChursaard <jame.paladin26222@gmail.com>
2022-05-17 15:22:23 +0700

    edit endcredit

--- src/endcredit.js
+++ src/endcredit.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 function EndCredit() {
   return (
     <div>
-      <h1> Thank you for participating. </h1>
+      <h1> Thank you for Testing. </h1>
       <h2> If You found bug. Report https://github.com/ThChursaard/EAQ </h2>
     </div>
   );
