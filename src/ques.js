5736d26d5c93ff87f093088ed54738d43ee8db2d
ThChursaard <jame.paladin26222@gmail.com>
2022-05-24 19:01:57 +0700

    add more q

--- src/ques.js
+++ src/ques.js
@@ -20,7 +20,18 @@ export const shuffle = (array) =>  {
               {q:'In the open air; Outside a building', a:'outdoor'},
               {q:'Physical activity that keep you fit', a:'exercise'},
               {q:'usual; happening often', a:'common'},
-              {q:'strong and well; not sick', a:'healthy'}
+              {q:'strong and well; not sick', a:'healthy'},
+              {q:'Common, regular', a:'ordinary'},
+              {q:'a chance to do something', a:'opportunity'},
+              {q:'things you do or that happen to you', a:'experiences'},
+              {q:'things you are able to do well', a:'skills'},
+              {q:'likely to cause harm or injury ', a:'dangerous'},
+              {q:'having imagination and artistic ability', a:'creative'},
+              {q:'to share information with other', a:'communicate'},
+              {q:'to go to different places and learn about thing', a:'explore'},
+              {q:'an exciting time or event', a:'adventure'},
+              {q:'preferred, most like ', a:'favorite'},
+              {q:'causing laughter or pleasure', a:'amusing'},
   
   ])
   
