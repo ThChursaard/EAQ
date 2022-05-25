dc3ad62b9e1e92b8934506730cb5454d8415131b
astralaria <pawut.j@gmail.com>
2022-05-25 18:51:56 +0700

    add questioncount

--- src/SelectQues.js
+++ src/SelectQues.js
@@ -4,17 +4,24 @@ import React, { useState } from "react";
 import { qa } from "./ques.js";
 import { shuffle } from "./ques.js";
 const ENDPOINT = "http://127.0.0.1:4001";
-
 const a = qa.map((c) => c.a);
-const sendQuestion = (question, answer) => {
-  fetch(`${ENDPOINT}/addquestion/?question=${question}&answer=${answer}`, { method: "GET" });
-};
 function SelectQues() {
+  const [questionCount, setQuestionCount] = useState(0);
   const [question, setQuestion] = useState(shuffle(a));
+
+  const sendQuestion = (question, answer) => {
+    if (questionCount < 10) {
+      fetch(`${ENDPOINT}/addquestion/?question=${question}&answer=${answer}`, {
+        method: "GET",
+      });
+      setQuestionCount(questionCount + 1);
+    }
+  };
   return (
     <div>
+      <div>{questionCount}/10</div>
       {qa.map((c) => (
-        <button onClick = {(e) => sendQuestion(c.q,c.a)} > {c.a} </button> 
+        <button onClick={(e) => sendQuestion(c.q, c.a)}> {c.a} </button>
       ))}
     </div>
   );
